7e73c008145abaf70f24c9487ddcdd6dbf1c29d5
Mohd Ali Ibrahim <mahziyar110@gmail.com>
2023-03-25 17:03:54 +0530

    adding pwa support to calc

---
+++ calc/service-worker.js
@@ -0,0 +1,42 @@
+const cacheName = "calc";
+
+const cacheAssets = ["/", "/index.html", "/cal.css", "/cal.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then((cache) => {
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(cacheName).then((cache) => {
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((res) => res))
+  );
+});
